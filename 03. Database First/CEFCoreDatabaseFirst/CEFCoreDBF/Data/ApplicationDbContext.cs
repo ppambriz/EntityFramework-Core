5b9aac035f43523533b133d5b678647b34381dc3
José Antonio Ambriz Hernández <jambriz@sapal.gob.mx>
2026-06-11 16:16:21 -0600

    Sesion 3 capitulo25 min 24

---
+++ 03. Database First/CEFCoreDatabaseFirst/CEFCoreDBF/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using CEFCoreDBF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CEFCoreDBF.Data;
+
+public partial class ApplicationDbContext : DbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<categoria> categoria { get; set; }
+
+    public virtual DbSet<nota> notas { get; set; }
+
+    public virtual DbSet<usuario> usuarios { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("Modern_Spanish_CI_AS");
+
+        modelBuilder.Entity<categoria>(entity =>
+        {
+            entity.Property(e => e.activo).HasDefaultValue(true, "DF_categoria_activo");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ 03. Database First/CEFCoreDatabaseFirst/CEFCoreDBF/Models/categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CEFCoreDBF.Models;
+
+public partial class categoria
+{
+    [Key]
+    public int idcategoria { get; set; }
+
+    [StringLength(100)]
+    public string nombre { get; set; } = null!;
+
+    [StringLength(250)]
+    public string? descripcion { get; set; }
+
+    public bool activo { get; set; }
+}
